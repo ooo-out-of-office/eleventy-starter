8fc3f77aa2b88ba62c96a50d6534af7d89facde8
Zack Seuberling <seuberling@gmail.com>
2021-02-08 14:45:36 -0800

    fix: tailwind css output

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  purge: ["./src/site/**/*.html", "./src/site/**/*.njk", "./src/site/**/*.md"],
   future: {
+    purgeLayersByDefault: true,
     removeDeprecatedGapUtilities: true,
   },
+  purge: ["./site/**/*.html", "./site/**/*.njk", "./site/**/*.md"],
   theme: {
     screens: {
       md: "768px",
@@ -22,53 +23,6 @@ module.exports = {
         400: "#bdbdbd",
       },
     },
-    backgroundColor: (theme) => theme("colors"),
-    borderColor: (theme) => ({
-      ...theme("colors"),
-      default: "currentColor",
-    }),
-    borderRadius: {
-      none: "0",
-    },
-    borderWidth: (theme) => ({
-      ...theme("spacing"),
-      default: "1px",
-    }),
-    fontFamily: {
-      sans: [
-        "Inter",
-        "Sen",
-        "system-ui",
-        "-apple-system",
-        "BlinkMacSystemFont",
-        '"Segoe UI"',
-        "Roboto",
-        '"Helvetica Neue"',
-        "Arial",
-        '"Noto Sans"',
-        "sans-serif",
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-        '"Noto Color Emoji"',
-      ],
-      serif: [
-        "Unna",
-        "Georgia",
-        "Cambria",
-        '"Times New Roman"',
-        "Times",
-        "serif",
-      ],
-      mono: [
-        "Menlo",
-        "Monaco",
-        "Consolas",
-        '"Liberation Mono"',
-        '"Courier New"',
-        "monospace",
-      ],
-    },
     fontSize: {
       base: "20px",
       sm: "16px",
@@ -79,32 +33,13 @@ module.exports = {
       normal: "400",
       bold: "700",
     },
-    height: (theme) => ({
-      auto: "auto",
-      full: "100%",
-      screen: "100vh",
-    }),
-    inset: (theme) => ({
-      ...theme("spacing"),
-      0: "0",
-      auto: "auto",
-    }),
     lineHeight: {
       normal: "calc(1em + 6px)",
     },
-    margin: (theme) => ({
-      ...theme("spacing"),
-      auto: "auto",
-    }),
     maxHeight: {
       full: "100%",
       screen: "100vh",
     },
-    maxWidth: (theme, { breakpoints }) => ({
-      none: "none",
-      full: "100%",
-      ...breakpoints(theme("screens")),
-    }),
     minHeight: {
       0: "0",
       full: "100%",
@@ -114,8 +49,6 @@ module.exports = {
       0: "0",
       full: "100%",
     },
-
-    padding: (theme) => theme("spacing"),
     spacing: {
       0: "0",
       1: "1px",
@@ -124,19 +57,39 @@ module.exports = {
       lg: "24px",
       xl: "30px",
     },
-    width: (theme) => ({
-      auto: "auto",
-      full: "100%",
-      screen: "100vw",
-    }),
-    zIndex: {
-      0: "0",
-      10: "10",
-      20: "20",
+    extend: {
+      borderWidth: {
+        default: "1px",
+      },
+      fontFamily: {
+        sans: [
+          "Inter",
+          "Sen",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          "Roboto",
+          '"Helvetica Neue"',
+          "Arial",
+          '"Noto Sans"',
+          "sans-serif",
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
+      height: {
+        auto: "auto",
+        full: "100%",
+        screen: "100vh",
+      },
+      maxWidth: {
+        none: "none",
+        full: "100%",
+      },
     },
-    gap: (theme) => ({
-      ...theme("spacing"),
-    }),
   },
   variants: {},
   plugins: [],
